84554e3347ec4a91838d98e67fb4517cd3da6c7a
Matt Bowden <matthew.bowden97@yahoo.co.uk>
2022-01-20 23:02:13 +0000

    Table sorting, API call & general code cleanup

--- public/js/main.js
+++ public/js/main.js
@@ -23,3 +23,199 @@ text = 'textContent' in document ? 'textContent' : 'innerText';
 for (var i = 1, len = rows.length; i < len; i++) {
     rows[i].children[0][text] = i + '' + rows[i].children[0][text];
 }
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("scores");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+
+function sortTableDifficulty() {
+  var table, rows, switching, i, current, next, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("scores");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      current = rows[i].getElementsByTagName("TD")[2];
+      next = rows[i + 1].getElementsByTagName("TD")[2];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+
+      //console.log("Current: " + current.innerHTML + " Next: " + next.innerHTML);
+      if (dir == "asc") {
+        if (current.innerHTML == "Hard" && next.innerHTML == "Medium") {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+        if (current.innerHTML == "Hard" && next.innerHTML == "Easy") {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+        if (current.innerHTML == "Medium" && next.innerHTML == "Easy") {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (current.innerHTML == "Easy" && next.innerHTML == "Medium") {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+        if (current.innerHTML == "Easy" && next.innerHTML == "Hard") {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+        if (current.innerHTML == "Medium" && next.innerHTML == "Hard") {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+
+function sortTableScore() {
+  var table, rows, switching, i, current, next, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("scores");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      current = parseInt(rows[i].getElementsByTagName("TD")[3].innerHTML);
+      next = parseInt(rows[i + 1].getElementsByTagName("TD")[3].innerHTML);
+
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        //console.log("[ASC] Current: " + current + " Next: " + next);
+        if (current > next) {
+          // If so, mark as a switch and break the loop:
+          //console.log("switch");
+          shouldSwitch = true;
+          break;
+        }
+        else
+        {
+          console.log("no need to switch");
+        }
+      } else if (dir == "desc") {
+        //console.log("[DESC] Current: " + current + " Next: " + next);
+        if (current < next) {
+          // If so, mark as a switch and break the loop:
+          //console.log("switch");
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
--- src/Controller/ScoresController.php
+++ src/Controller/ScoresController.php
@@ -9,6 +9,8 @@
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\Form\Extension\Core\Type\TextType;
+    use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+    use Symfony\Component\Form\Extension\Core\Type\IntegerType;
     use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
     class ScoresController extends AbstractController {
@@ -21,10 +23,13 @@
 
             $entityManager = $this->getDoctrine()->getManager();
 
+            //Build the form used to add a new score
             $form = $this->createFormBuilder($score)
                 ->add('name', TextType::class, array('attr' => array('class' => 'form-control')))
-                ->add('difficulty', TextType::class, array('attr' => array('class' => 'form-control')))
-                ->add('score', TextType::class, array('attr' => array('class' => 'form-control')))
+                ->add('difficulty', ChoiceType::class, array(
+                    'attr' => array('class' => 'form-control'),
+                    'choices' => array('Easy' => 'Easy', 'Medium' => 'Medium', 'Hard' => 'Hard')))
+                ->add('score', IntegerType::class, array('attr' => array('class' => 'form-control')))
                 ->add('save', SubmitType::class, array(
                     'label' => 'Save new Score',
                     'attr' => array('class' => 'btn btn-primary mt-3')))
@@ -32,24 +37,27 @@
 
             $form->handleRequest($request);
 
+            //If data is submitted, create a new score entry
             if($form->isSubmitted() && $form->isValid()){
                 $score = $form->getData();
-
                 $score->setAuthorised(false);
-
                 $entityManager->persist($score);
-
                 $entityManager->flush();
             }
+            
+            //Get JSON data via API call
+            $url = $this->getParameter(app.apiurl);             //API URL. CHANGE IN .env FOR YOUR LOCAL INSTALL
+            $data = ['collection' => 'scores'];                 //Collection object
+            $curl = curl_init($url);                            //Initializes a new cURL session
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);   //Set the CURLOPT_RETURNTRANSFER option to true
+            curl_setopt($curl, CURLOPT_POST, false);            //Set the CURLOPT_POST option to false for GET request
+            $response = curl_exec($curl);                       //Execute cURL request with all previous settings
+            curl_close($curl);                                  //Close cURL session
 
-            $qb = $entityManager->createQueryBuilder()
-                ->select('s')
-                ->from(Score::class, 's')
-                ->where('s.authorised = 1')
-                ->orderBy('s.score', 'DESC');
+            //Convert JSON into object
+            $scores = json_decode($response);
 
-            $scores = $qb->getQuery()->getResult();
-
+            //Show the user the authorised scores page
             return $this->render('scores/index.html.twig', array('admin' => false, 'scores' => $scores, 'form' => $form->createView()));
         }
 
@@ -58,8 +66,30 @@
         * @Method({"GET"})
         */
         public function admin(){
+            //Get all scores
             $scores = $this->getDoctrine()->getRepository(Score::class)->findBy(array(), array('score' => 'DESC'));
+
+            //Show the admin all the scores
             return $this->render('scores/index.html.twig', array('admin' => true, 'scores' => $scores));
+        }
+
+        /**
+        * @Route("/score/data")
+        * @Method({"GET"})
+        */
+        public function data(){
+            //Get scores in descending order
+            $entityManager = $this->getDoctrine()->getManager();
+            $qb = $entityManager->createQueryBuilder()
+                ->select('s')
+                ->from(Score::class, 's')
+                ->where('s.authorised = 1')
+                ->orderBy('s.score', 'DESC');
+            $scores = $qb->getQuery()->getResult();
+
+            //Encode into JSON
+            $json = json_encode($scores);
+            return new Response($json);
         }
 
         /**
@@ -67,12 +97,15 @@
          * @Method({"DELETE"})
          */
         public function delete(Request $request, $id){
+            //Get the individual score
             $score = $this->getDoctrine()->getRepository(Score::class)->find($id);
 
+            //Remove it
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($score);
             $entityManager->flush();
 
+            //Send a response
             $response = new Response();
             $response->send();
         }
@@ -81,31 +114,16 @@
          * @Route("/score/authorise/{id}")
          */
         public function authorise($id){
+            //Get the individual score
             $score = $this->getDoctrine()->getRepository(Score::class)->find($id);
             $entityManager = $this->getDoctrine()->getManager();
+            
+            //Change authorised to true (1)
             $score->setAuthorised(true);
             $entityManager->flush();
 
+            //Redirect to see changes
             return $this->redirect("/admin");
-        }
-
-        /**
-         * @Route("/score/save")
-         */
-        public function save(){
-            /*
-            $entityManager = $this->getDoctrine()->getManager();
-            $score = new Score();
-            $score->setName("John");
-            $score->setDifficulty("Medium");
-            $score->setScore("10000");
-            $score->setAuthorisation(true);
-
-            $entityManager->persist($score);
-            $entityManager->flush();
-
-            return new Response("Saved data with ID: " . $score->getId());
-            */
         }
     }
 ?>
